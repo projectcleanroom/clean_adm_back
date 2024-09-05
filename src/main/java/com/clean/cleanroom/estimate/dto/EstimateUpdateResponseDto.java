1551f6d090bad86c4dd4d0e469ce1261360d1ceb
hajin428 <amm_321@naver.com>
2024-09-05 21:01:41 +0900

    refactor : 견적 DTO 스키마 추가

--- src/main/java/com/clean/cleanroom/estimate/dto/EstimateUpdateResponseDto.java
+++ src/main/java/com/clean/cleanroom/estimate/dto/EstimateUpdateResponseDto.java
@@ -3,9 +3,11 @@ package com.clean.cleanroom.estimate.dto;
 import com.clean.cleanroom.commission.entity.Commission;
 import com.clean.cleanroom.enums.CleanType;
 import com.clean.cleanroom.enums.HouseType;
+import com.clean.cleanroom.enums.StatusType;
 import com.clean.cleanroom.estimate.entity.Estimate;
 import com.clean.cleanroom.members.entity.Address;
 import com.clean.cleanroom.members.entity.Members;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -15,17 +17,41 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class EstimateUpdateResponseDto {
 
-    private Long id;
+    @Schema(description = "견적 식별값")
+    private long id;
+
+    @Schema(description = "의뢰 식별값")
     private Long commissionId;
+
+    @Schema(description = "파트너 식별값")
     private Long partnerId;
-    private int Price;
-    private LocalDateTime fixedDate; // 확정일자
+
+    @Schema(description = "임시 가격")
+    private int price;
+
+    @Schema(description = "확정 일자", example = "2024-09-05T10:00:00")
+    private LocalDateTime fixedDate;
+
+    @Schema(description = "코멘트")
     private String statement;
+
+    @Schema(description = "회원 닉네임")
     private String nick;
+
+    @Schema(description = "주소")
     private String address;
+
+    @Schema(description = "주거 형태")
     private HouseType houseType;
+
+    @Schema(description = "청소 타입")
     private CleanType cleanType;
+
+    @Schema(description = "희망 일자", example = "2024-09-05T10:00:00")
     private LocalDateTime desiredDate; // 희망날짜
+
+    @Schema(description = "견적 상태")
+    private StatusType status;
 
     public EstimateUpdateResponseDto (Estimate estimate,
                                      Members members,
@@ -33,7 +59,7 @@ public class EstimateUpdateResponseDto {
                                      Commission commission) {
         this.commissionId = estimate.getCommission().getId();
         this.partnerId = estimate.getPartner().getId();
-        this.Price = estimate.getPrice();
+        this.price = estimate.getPrice();
         this.fixedDate = estimate.getFixedDate();
         this.statement = estimate.getStatement();
         this.nick = members.getNick();
