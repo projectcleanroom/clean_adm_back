7bf5aaa1d7bb338bff5b563fa92b41085dad3919
hajin428 <amm_321@naver.com>
2024-09-17 00:08:36 +0900

    feat : redis 도입과 견적 내역 조회 API에 레디스 적용

--- src/main/java/com/clean/cleanroom/business/entity/Business.java
+++ src/main/java/com/clean/cleanroom/business/entity/Business.java
@@ -2,7 +2,6 @@ package com.clean.cleanroom.business.entity;
 
 
 import com.clean.cleanroom.members.entity.Members;
-import com.clean.cleanroom.partner.entity.Partner;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,34 +15,36 @@ public class Business {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // 사업자 정보 식별값
+    private Long id;
 
     @Column(nullable = false)
-    private String businessNumber; // 사업자등록번호
+    private String businessNumber;
 
     @Column(nullable = false)
-    private LocalDate startDate; // 개업일
+    private LocalDate startDate;
 
     @Column(nullable = false)
-    private String representativeName; // 대표자 설명
-
-    @Column(nullable = false)
-    private String companyName; // 업체명
+    private String representativeName;
 
     @Column
-    private String businessSector; // 사업 부문
+    private String additionalRepresentativeName;
 
-    @Column
-    private String businessType; // 업종
-
-    // 사업자 등록번호 진위 여부 확인 결과
     @Column(nullable = false)
-    private boolean isValid; // true - 정상, false - 오류
+    private String companyName;
 
     @Column
-    private LocalDate validationDate; // 검증된 날짜
+    private String corporationNumber;
+
+    @Column
+    private String businessSector;
+
+    @Column
+    private String businessType;
+
+    @Column
+    private String businessAddress;
 
     @ManyToOne
-    @JoinColumn(name = "mambers_id")
-    private Members members; // 연관된 파트너
+    @JoinColumn(name = "members_id")
+    private Members members;
 }
